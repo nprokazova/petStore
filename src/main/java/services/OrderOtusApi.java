b2ef64bfc3b30bc256e50b24d791c0df7eef1129
nprokazova <naprokazova@gmail.com>
2021-07-24 22:41:58 +0300

    Домашнее задание Rest-assured

---
+++ src/main/java/services/OrderOtusApi.java
@@ -0,0 +1,39 @@
+package services;
+
+import dto.OtusOrder;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+import static io.restassured.RestAssured.given;
+
+public class OrderOtusApi {
+    private static final String BASE_URI = "https://petstore.swagger.io/v2/";
+    private RequestSpecification spec;
+    private static final String ORDER = "/store/order/";
+
+    public OrderOtusApi() {
+        spec = given()
+                .baseUri(BASE_URI)
+                .contentType(ContentType.JSON);
+    }
+
+    public Response createOrder(OtusOrder order) {
+        return
+                given(spec)
+                        .with()
+                        .body(order)
+                        .log().all()
+                        .when()
+                        .post(ORDER);
+    }
+
+    public Response checkOrder(Long id) {
+        return
+                given(spec)
+                        .with()
+                        .log().all()
+                        .when()
+                        .get(ORDER + id);
+    }
+}
